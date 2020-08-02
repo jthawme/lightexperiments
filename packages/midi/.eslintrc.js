692ca9967cbdccb357257243280f1711337511c5
Jonny Thaw <hi@jthaw.me>
2020-08-02 22:44:20 +0100

    initial commit

---
+++ packages/midi/.eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    es2020: true,
+    node: true,
+  },
+  extends: ["eslint:recommended", "google", "prettier"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {},
+};
