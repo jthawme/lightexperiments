135533fbc5a5a79bcc9253313c8660cc21ec7489
Jonny Thaw <hi@jthaw.me>
2020-08-03 00:23:00 +0100

    feat: added socket too

--- packages/app/src/index.ts
+++ packages/app/src/index.ts
@@ -1,15 +1,27 @@
 import express from "express";
-import { log } from "@light-experiments/config";
+import http from "http";
 import open from "open";
+import socketIO from "socket.io";
+import { log } from "@light-experiments/config";
 
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
 const port = 8080;
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-app.listen(port, () => {
+io.on("connection", (socket) => {
+  log("a user connected");
+
+  socket.on("message", (msg) => {
+    log("msg: " + msg);
+  });
+});
+
+server.listen(port, () => {
   log("Server up and running");
   open(`http://localhost:${port}`);
 });
